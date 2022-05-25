5f244bf8df760b79870528035fb3c5793cdbb87f
Анатолий <tolyan011998@gmail.com>
2022-05-25 20:37:53 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxtomcAhqHu3X2B8Haxa3bK3mXFYqhMtDYJg&usqp=CAU" alt="logo"/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
+      </div>
+      <div>
+        <img src="https://ps.w.org/tiny-compress-images/assets/icon-256x256.png?rev=1088385" alt=""/>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          News post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
