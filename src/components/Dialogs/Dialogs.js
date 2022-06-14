b1fb9abd7b494bbfcc6be0a8aa19fa23cf7f82f1
crypton <tolyan011998@gmail.com>
2022-06-14 15:35:12 +0300

    add data for dialogs and posts

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,8 +1,47 @@
 import React from "react";
+import classes from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+  const path = `/dialogs/${props.id}`
+
+  return (
+    <div className={`${classes.dialog} ${classes.active}`}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  )
+}
 
 const Dialogs = (props) => {
+  const dialogsData = [
+    {id: 1, name: 'Tolya'},
+    {id: 2, name: 'Nikita'},
+    {id: 3, name: 'Serega'},
+    {id: 4, name: 'Shamil'},
+    {id: 5, name: 'Kolya'}
+  ]
+
+  const messagesData = [
+    {id: 1, message: 'Hello'},
+    {id: 2, message: 'How are you'},
+    {id: 3, message: 'What is your name'},
+  ]
+
     return (
-        <h2>Dialogs</h2>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+              <DialogItem name='Tolya' id='1' />
+              <DialogItem name='Nikita' id='2' />
+              <DialogItem name='Serega' id='3' />
+              <DialogItem name='Shamil' id='4' />
+              <DialogItem name='Kolya' id='5' />
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>How are you</div>
+                <div className={classes.message}>What is your name</div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,19 +1,29 @@
 import React from "react";
 import Post from "./Post/Post";
+import classes from "./MyPosts.module.css"
 
 const MyPosts = () => {
-    return (
-        <div>
-            My posts
-            <div>
-                News post
-            </div>
-            <div>
-                <Post message='Hello, how are you'/>
-                <Post message='It`s my first post'/>
-            </div>
-        </div>
-    )
+  const postsData = [
+    {id: 1, message: 'Hello, how are you', likeCount: 12},
+    {id: 2, message: 'It`s my first post', likeCount: 23}
+  ]
+
+  return (
+    <div className={classes.posts}>
+      <h3>My posts</h3>
+      <div>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+      </div>
+      <div>
+        <button>Add new post</button>
+      </div>
+
+      <div>
+        <Post message='Hello, how are you' likeCount="12"/>
+        <Post message='It`s my first post' likeCount="124"/>
+      </div>
+    </div>
+  )
 }
 
 export default MyPosts
