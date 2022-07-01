88d0a6d9801267f4f0401a189ab4fd2a7eb76109
crypton <tolyan011998@gmail.com>
2022-07-01 15:51:43 +0300

    add get request in componentDidMount

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,37 +1,39 @@
 import React from "react";
 import * as axios from 'axios'
 
-const Users = (props) => {
-  if (!props.users.length) {
+class Users extends React.Component {
+  componentDidMount() {
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-      console.log('response: ', response)
-      props.setUsers(response.data.items)
+      console.log('qwqw')
+      this.props.setUsers(response.data.items)
     })
   }
 
-  return <div>
-    {
-      props.users.map(user => <div key={user.id}>
-          <div>
-            <img src={user.photos.small} alt=""/>
-            {user.followed
-              ? <button onClick={() => {props.follow(user.id)}}>Follow</button>
-              : <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button>}
-          </div>
-          <div>
+  render = () => {
+    return <div>
+      {
+        this.props.users.map(user => <div key={user.id}>
             <div>
-              <p>{user.name}</p>
-              <p>{user.status}</p>
+              <img src={user.photos.small} alt=""/>
+              {user.followed
+                ? <button onClick={() => {this.props.follow(user.id)}}>Follow</button>
+                : <button onClick={() => {this.props.unfollow(user.id)}}>Unfollow</button>}
             </div>
             <div>
-              <span>{"user.location.city"}</span>
-              <span>{"user.location.country"}</span>
+              <div>
+                <p>{user.name}</p>
+                <p>{user.status}</p>
+              </div>
+              <div>
+                <span>{"user.location.city"}</span>
+                <span>{"user.location.country"}</span>
+              </div>
             </div>
           </div>
-        </div>
-      )
-    }
-  </div>
+        )
+      }
+    </div>
+  }
 }
 
 export default Users
